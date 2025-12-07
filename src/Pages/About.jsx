66f8754d655462a4e3f02cebb7bc170ffe51f18a
Vinay-278 <vk0565106@gmail.com>
2025-12-07 22:12:09 +0530

    first commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -49,14 +49,14 @@ const About = () => {
               <h1 className="bg-gradient-to-br from-[#833ab4] via-[#fd1d1d] to-[#fcb045] bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%]">
                 Our Founding Story
               </h1>
-              <p className="text-base font-medium text-[#838894] lg:w-[95%]">
+              <p className="font-medium text-[#838894] lg:w-[95%]">
                 Our e-learning platform was born out of a shared vision and
                 passion for transforming education. It all began with a group of
                 educators, technologists, and lifelong learners who recognized
                 the need for accessible, flexible, and high-quality learning
                 opportunities in a rapidly evolving digital world.
               </p>
-              <p className="text-base font-medium text-[#838894] lg:w-[95%]">
+              <p className="font-medium text-[#838894] lg:w-[95%]">
                 As experienced educators ourselves, we witnessed firsthand the
                 limitations and challenges of traditional education systems. We
                 believed that education should not be confined to the walls of a
@@ -76,11 +76,11 @@ const About = () => {
           </div>
         </div>
         <div className="flex flex-col items-center lg:gap-5 lg:flex-row justify-between">
-          <div className="my-24 flex lg:w-[40%]  flex-col gap-10">
+          <div className="px-12 my-24 flex lg:w-[40%]  flex-col gap-10">
             <h1 className="bg-gradient-to-b from-[#ff512f] to-[#f09819] bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%]">
               Our Vision
             </h1>
-            <p className="text-base font-medium text-[#838894] lg-w-[95%]">
+            <p className="font-medium text-[#838894] lg:w-[95%]">
               With this vision in mind, we set out on a journey to create an
               e-learning platform that would revolutionize the way people learn.
               Our team of dedicated experts worked tirelessly to develop a
@@ -89,11 +89,11 @@ const About = () => {
               interactive learning experience.
             </p>
           </div>
-          <div className="my-24 flex lg:w-[40%] flex-col gap-10">
+          <div className="my-24 px-12 flex lg:w-[40%] flex-col gap-10">
             <h1 className="bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] text-transparent bg-clip-text text-4xl font-semibold lg:w-[70%]">
               Our Mission
             </h1>
-            <p className="text-base font-medium text-[#838894] lg:w-[95%]">
+            <p className="font-medium text-[#838894] lg:w-[95%]">
               Our mission goes beyond just delivering courses online. We wanted
               to create a vibrant community of learners, where individuals can
               connect, collaborate, and learn from one another. We believe that
@@ -104,6 +104,15 @@ const About = () => {
           </div>
         </div>
       </section>
+      <Stats />
+      <section className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white">
+        <LearningGrid />
+        <ContactFormSection />
+      </section>
+      <div className="realative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-[#000814] text-white">
+        <ReviewSlider />
+      </div>
+      {/* <Footer/> */}
     </div>
   );
 }
