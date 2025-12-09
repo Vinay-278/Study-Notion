4b27a97e380829537ca6093df48124478ce2318e
Vinay-278 <vk0565106@gmail.com>
2025-12-09 23:47:24 +0530

    first

--- src/Components/ContactPage/ContactDetails.jsx
+++ src/Components/ContactPage/ContactDetails.jsx
@@ -1,11 +1,76 @@
 import React from 'react'
+import * as Icon1 from "react-icons/bi"
+import * as Icon3 from "react-icons/hi2"
+import * as Icon2 from "react-icons/io5"
+
+const contactDetails = [
+  {
+    icon: "HiChatBubbleLeftRight",
+    heading: "Chat with us",
+    description: "Our friendly team is here to help.",
+    details: "info@studynotion.com",
+    linkType: "email",
+  },
+  {
+    icon: "BiWorld",
+    heading: "Visit us",
+    description: "Come and say hello at our office HQ.",
+    details:
+      "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+    linkType: null,
+  },
+  {
+    icon: "IoCall",
+    heading: "Call us",
+    description: "Mon - Fri From 8am to 5pm",
+    details: "+123 456 7869",
+    linkType: "phone",
+  },
+];
 
 const ContactDetails = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-6 p-[34px] rounded-xl bg-[#161D29] h-[500px] lg:p-6">
+      {
+        contactDetails.map((ele,i)=>{
+          let Icon= Icon1[ele.icon] || Icon2[ele.icon] || Icon3[ele.icon];
+          let detailElement;
+          if(ele.linkType === "email"){
+              detailElement =(
+                <a href={`mailto:${ele.details}`} className='font-semibold text-[]'>
+                  {ele.details}
+                </a>
+              )
+          }
+          else if(ele.linkType === "phone"){
+              detailElement = (
+                <a
+                  href={`tel:${ele.details}`}
+                  className="font-semibold text-[#999DAA]"
+                >
+                  {ele.details}
+                </a>
+              );
+          }
+          else{
+            detailElement = <p className='font-semibold'>{ele.details}</p>
+          }
+          return (
+            <div className="flex flex-col p-3 text-sm text-[#999DAA]" key={i}>
+              <div className="flex flex-row items-center gap-3">
+                <Icon size={25} />
+                <h1 className="text-lg font-semibold text-[#999DAA]">
+                  {ele.heading}
+                </h1>
+              </div>
+              <p className="font-medium">{ele?.description}</p>
+              {detailElement}
+            </div>
+          );
+        })
+      }
     </div>
-  )
+  );
 }
 
 export default ContactDetails
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Footer from '../Components/Common/Footer'
 import ReviewSlider from '../Components/Common/ReviewSlider'
 import ContactDetails from '../Components/ContactPage/ContactDetails'
@@ -8,7 +7,21 @@ import ContactForm from '../Components/ContactPage/ContactForm'
 const Contact = () => {
   return (
     <div>
-      
+      <div className='mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row'>
+      {/* Contact Details */}
+      <div className='lg:w-[40%]'>
+        <ContactDetails/>
+      </div>
+      {/* Contact form */}
+      <div className='lg:w-[60%]'>
+        <ContactForm/>
+      </div>
+      </div>
+      <div className='relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white'>
+      {/* Review from other learner */}
+        <ReviewSlider/>
+      </div>
+      {/* <Footer/> */}
     </div>
   )
 }
