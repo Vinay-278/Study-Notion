3295a380f76d4ee636789995f4e97dcb5be4a85d
Vinay-278 <vk0565106@gmail.com>
2025-12-10 22:53:32 +0530

    first commit

--- src/Components/ContactPage/ContactForm.jsx
+++ src/Components/ContactPage/ContactForm.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import ContactUsForm from './ContactUsForm'
 
 const ContactForm = () => {
   return (
-    <div>
-      
+    <div className="border border-[#424854] text-[#838894] rounded-xl p-7 lg:p-14 flex gap-3 flex-col">
+      <div className="text-4xl font-semibold text-shadow-white">
+        Got an Idea? We&apos; we got the skills. Let&apos;s team up !!
+      </div>
+      <p className='font-medium text-[17px]'>Tell us more about yourself and what you&apos;re got in mind.</p>
+      <div className="mt-7">
+        <ContactUsForm />
+      </div>
     </div>
-  )
+  );
 }
 
 export default ContactForm
--- src/Components/ContactPage/ContactUsForm.jsx
+++ src/Components/ContactPage/ContactUsForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const ContactUsForm = () => {
   return (
     <div>
