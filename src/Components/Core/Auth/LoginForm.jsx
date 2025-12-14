3200f818f1d48c9817d2a22ffeda54e679c3be8b
Vinay-278 <vk0565106@gmail.com>
2025-12-15 04:50:16 +0530

    first commit

--- src/Components/Core/Auth/LoginForm.jsx
+++ src/Components/Core/Auth/LoginForm.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router"
 import { SiGmail } from "react-icons/si"
 import { FaLock } from "react-icons/fa"
 import { useState } from "react"
-import {} from "../../../Service"
+import {login} from "../../../Service/operations/authAPI"
 
 const LoginForm = () => {
     const navigate = useNavigate();
@@ -27,10 +27,83 @@ const LoginForm = () => {
     }
 
   return (
-    <form onSubmit={handleOnChange} className="mt-6 flex">
-      
+    <form
+      onSubmit={handleOnChange}
+      className="mt-6 flex w-full flex-col gap-y-4"
+    >
+      <label className="w-full" style={{ position: "relative" }}>
+        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-white">
+          Email Address <sup className="text-pink-300">*</sup>
+        </p>
+        <SiGmail
+          style={{
+            color: "grey",
+            position: "absolute",
+            top: "calc(49%)",
+            left: "1.65%",
+            fontSize: "20px",
+          }}
+        />
+        <input
+          required
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleOnChange}
+          placeholder="Enter email address"
+          style={{
+            boxShadow: "inset 0px -1px 0px rgba(255,255,255,0.18)",
+            paddingLeft: "35px",
+          }}
+          className="w-full rounded-[0.5rem] bg-[] text-white"
+        />
+      </label>
+      <label className="relative">
+        <FaLock
+          style={{
+            color: "grey",
+            position: "absolute",
+            top: "calc(42%)",
+            left: "1.65%",
+            fontSize: "20px",
+          }}
+        />
+        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-white">
+          Password <sup className="text-pink-300">*</sup>
+        </p>
+        <input
+          required
+          type={showPassword ? "text" : "password"}
+          name="password"
+          value={password}
+          onChange={handleOnChange}
+          placeholder="Enter password"
+          style={{
+            boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+            paddingLeft: "35px",
+          }}
+          className="w-full rounded-[0.5rem] bg-[] p-[12px] pr-12 text-white"
+        /> 
+        <span onClick={()=>setShowPassword((prev)=>!prev)} className="absolute right-3 top-[38px] z-[10] cursor-pointer">
+            {
+                showPassword? (
+                    <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF"/>
+                ):(
+                    <AiOutlineEye fontSize={24} fill="#AFB2BF"/>
+                )
+            }
+        </span>
+        <Link to="/forgot-password">
+            <p className="mt-1 ml-auto max-w-max text-xs text-blue-300">
+                Forgot Password
+            </p>
+        </Link>
+      </label>
+      <button type="submit" className="mt-6 rounded-[8px] bg-yellow-300 py-[8px] px-[12px] font-medium text-[]">
+        Sign In
+      </button>
     </form>
-  )
+  );
 }
 
 export default LoginForm
